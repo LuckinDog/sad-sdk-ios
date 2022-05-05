66aa4a68b562d64a340998b81c7f7a7a1d9f77eb
Jack Chen <48739899+LuckinDog@users.noreply.github.com>
2022-05-05 18:00:11 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,13 +10,21 @@ let package = Package(
     products: [
         .library(
             name: "SensorsAdvertising",
-            targets: ["SensorsAdvertising"]),
+            targets: ["SensorsAdvertisingWrapper"]),
     ],
     dependencies: [.package(name: "SensorsAnalyticsSDK", url: "https://github.com/sensorsdata/sa-sdk-ios-spm.git", from: "4.3.0")],
     targets: [
         .binaryTarget(
             name: "SensorsAdvertising",
             path: "SensorsAdvertising_AdServices/SensorsAdvertising.xcframework"
+        ),
+        .target(
+            name: "SensorsAdvertisingWrapper",
+            dependencies: [
+                .target(name: "SensorsAdvertising"),
+                .product(name: "SensorsAnalyticsSDK", package: "SensorsAnalyticsSDK")
+            ],
+            path: "SensorsAdvertising_AdServices"
         )
     ]
 )
